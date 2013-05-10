f56c72bbb9b5caea263139cbfe63ee0a600bca8d
dolby <rob.jones@dolby.com>
2013-05-10 15:17:14 +1000

    Dolby changes for CAF AudioPolicyManagerALSA UDC support
    Change-Id: I4d70bed3892bbd8a326af4908334c1afc7395326
    Signed-off-by: Paul Keith <javelinanddart@gmail.com>

--- legacy/msm8660/Android.mk
+++ legacy/msm8660/Android.mk
@@ -1,3 +1,24 @@
+/*
+ * This file was modified by Dolby Laboratories, Inc. The portions of the
+ * code that are surrounded by "DOLBY..." are copyrighted and
+ * licensed separately, as follows:
+ *
+ * (C) 2012 Dolby Laboratories, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ */
+
 #AUDIO_POLICY_TEST := true
 #ENABLE_AUDIO_DUMP := true
 
@@ -100,6 +121,10 @@ include $(CLEAR_VARS)
 LOCAL_SRC_FILES := \
     AudioPolicyManager.cpp \
     audio_policy_hal.cpp
+
+ifeq ($(DOLBY_UDC_MULTICHANNEL),true)
+  LOCAL_CFLAGS += -DDOLBY_UDC_MULTICHANNEL
+endif /* DOLBY_UDC_MULTICHANNEL */
 
 LOCAL_SHARED_LIBRARIES := \
     libcutils \
